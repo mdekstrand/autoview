39062988d85fed23951b9b9473887e8b449954b8
Michael Ekstrand <md@ekstrandom.net>
2024-07-06 17:23:03 -0400

    start guessing file types

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::path::PathBuf;
+
 use anyhow::Result;
 use clap::{Args, Parser};
 use log::*;
 use stderrlog::StdErrLog;
+use xdg_mime::SharedMimeInfo;
 
 /// Automatically view files and file information.
 #[derive(Parser)]
@@ -17,18 +20,25 @@ struct CLI {
     /// Enable verbose diagnostic logging
     #[arg(short = 'v', long = "verbose", action = clap::ArgAction::Count)]
     verbose: u8,
+
+    /// File to display
+    file: PathBuf,
 }
 
 #[derive(Args)]
 #[group(multiple = false)]
 struct AVAction {
     /// Show only the first several lines of the file
-    #[arg(short = 'H', long = "head", group = "operation")]
+    #[arg(short = 'H', long = "head")]
     head: bool,
 
     /// Show the file's metadata
-    #[arg(short = 'M', long = "meta", group = "operation")]
+    #[arg(short = 'M', long = "meta")]
     meta: bool,
+
+    /// Display the file's MIME type and exit
+    #[arg(short = 'T', long = "mime-type")]
+    mime_type: bool,
 }
 
 fn main() -> Result<()> {
@@ -37,5 +47,14 @@ fn main() -> Result<()> {
         .verbosity(opts.verbose as usize + 1)
         .init()?;
     info!("CLI launching");
+
+    let db = SharedMimeInfo::new();
+    let guess = db.guess_mime_type().path(&opts.file).guess();
+    let mime = guess.mime_type();
+    println!("{}: {}", opts.file.display(), mime);
+    if guess.uncertain() {
+        println!("{}: guess is uncertain", opts.file.display());
+    }
+
     Ok(())
 }
